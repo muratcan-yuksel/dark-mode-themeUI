89b65c06e72cd9ab68047696c921833205efadb8
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-09-17 02:14:46 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+/** @jsxImportSource theme-ui */
+import { Button, Box } from "theme-ui";
+import { Switch } from "theme-ui";
+import { useColorMode } from "theme-ui";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [colorMode, setColorMode] = useColorMode();
+  const [state, setState] = useState("light");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="boxes">
+        <Box p={4} color="text" bg="transparent" className="box">
+          <div className="para">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
+            explicabo omnis? Laborum voluptatum explicabo unde quae totam,
+            reprehenderit omnis delectus expedita quas non corporis, veniam nisi
+            consectetur facilis, quam in.
+          </div>
+        </Box>{" "}
+        <Box p={4} color="text" bg="transparent" className="box">
+          <div className="para">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
+            explicabo omnis? Laborum voluptatum explicabo unde quae totam,
+            reprehenderit omnis delectus expedita quas non corporis, veniam nisi
+            consectetur facilis, quam in.
+          </div>
+        </Box>{" "}
+        <Box p={4} color="text" bg="transparent" className="box">
+          <div className="para">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
+            explicabo omnis? Laborum voluptatum explicabo unde quae totam,
+            reprehenderit omnis delectus expedita quas non corporis, veniam nisi
+            consectetur facilis, quam in.
+          </div>
+        </Box>
+      </div>
+      <div
+        className="para2"
+        sx={{ fontWeight: "900", color: "text", fontSize: "20px" }}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        qui dolorem eaque distinctio magni quasi deserunt ipsam possimus ea
+        facilis adipisci asperiores doloremque, natus totam sed, odio aliquam?
+        Nihil, ea.
+      </div>
+      {/* <button sx={{ bg: "primary", color: "inverseText" }}> Toggle</button>
+      <Button mr={2}>Beep</Button>
+      <Button variant="secondary">Boop</Button>
+      <Button ml={2} hidden>
+        Hidden
+      </Button> */}
+      <div className="wr">
+        <div>{state === "light" ? <div> Dark</div> : <div>Light</div>}</div>
+        <Switch
+          className="switch"
+          onClick={() => {
+            setColorMode(colorMode === "light" ? "dark" : "light");
+            setState(colorMode === "light" ? "dark" : "light");
+          }}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,20 @@
+// import type { Theme } from "theme-ui";
+const theme = {
+  initialColorMode: "light",
+  colors: {
+    text: "black",
+    inverseText: "white",
+    background: "white",
+    primary: "#1408e6",
+    modes: {
+      dark: {
+        text: "white",
+        inverseText: "black",
+        background: "#282c34",
+        primary: "#8fceff",
+      },
+    },
+  },
+};
+
+export default theme;
